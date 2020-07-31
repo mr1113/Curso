3882570dfd97256a017b81864d348c48e3323bdb
Mauricio <mauricioperez1399@gmail.com>
2020-07-31 14:23:39 -0500

    Curso

---
+++ DOM/js/scripts.js
@@ -0,0 +1,22 @@
+(function(){
+  'use strict';
+  document.addEventListener('DOMContentLoaded', function(){
+    
+    /* getElementById
+    var logo = document.getElementById("logo");
+    var navegacion = document.getElementById("navegacion");
+    navegacion.style.display ="none"; */
+    
+    /* getElementsByClassName
+    var navegacion = document.getElementsByClassName("navegacion");
+    console.log(navegacion);
+
+    var contenido = document.getElementsByClassName("contenido");
+    console.log(contenido);
+
+    contenido[0].style.display ="none"; */
+
+
+  });
+  
+})();
